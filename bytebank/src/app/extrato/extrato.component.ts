430465bad3bda61e9f7e58b7f288fbba60f02307
Hadnan Basilio <basiliohadnan@gmail.com>
2022-03-04 16:49:18 -0300

    Atualizando repo

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,6 @@
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Input, OnInit } from '@angular/core';
+import { Transferencia } from '../models/transferencia.models';
 
 @Component({
   selector: 'app-extrato',
@@ -6,10 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./extrato.component.scss']
 })
 export class ExtratoComponent implements OnInit {
-  @Input() transferencias: any[];
+  transferencias: any[];
 
-  constructor() { }
+  constructor(private service: TransferenciaService) { }
 
   ngOnInit(): void {
+    this.service.todas()
+      .subscribe((transferencias: Transferencia[]) => {
+        console.table(transferencias);
+        this.transferencias = transferencias;
+      })
   }
 }
