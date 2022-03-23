2fc1958b361cbb22355b5ab86a8be801acda8831
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-03-23 16:34:08 -0300

    Hoje foe

--- 002. Angular Boas práticas em arquiteturas e formulários/gatitobook/src/app/animais/animais.module.ts
+++ 002. Angular Boas práticas em arquiteturas e formulários/gatitobook/src/app/animais/animais.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
-import { ListaAnimaisComponent } from './animais/lista-animais/lista-animais.component';
+import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 
 
 @NgModule({
-  declarations: [
-    ListaAnimaisComponent
-  ],
+  declarations: [ListaAnimaisComponent],
   imports: [
     CommonModule,
     AnimaisRoutingModule
