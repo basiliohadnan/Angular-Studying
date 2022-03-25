d9ca3258b4d51fa14b200579b6d9100c945fc365
Hadnan Basilio | BomTrato <hbasilio@bomtrato.com>
2022-03-25 17:11:28 -0300

    Até que vai

---
+++ 002. Angular Boas práticas em arquiteturas e formulários/gatitobook/src/app/autenticacao/usuario/usuario.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { TokenService } from '../token.service';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private usuarioSubject = new BehaviorSubject<Usuario>({})
+
+  constructor(private tokenService: TokenService) { 
+    if (this.tokenService.possuiToken()) this.decodificaJWT
+  }
+
+  private decodificaJWT() {
+    const token = this.tokenService.retornaToken()
+    const usuario = jwtDecode(token) as Usuario
+    this.usuarioSubject.next(usuario)
+  }
+
+  retornaUsuario(){
+    return this.usuarioSubject.asObservable()
+  }
+
+  salvaToken(token: string){
+    this.tokenService.salvaToken(token)
+    this.decodificaJWT()
+  }
+}
--- 002. Angular Boas práticas em arquiteturas e formulários/gatitobook/src/app/home/novo-usuario/usuario-existe.service.ts
+++ 002. Angular Boas práticas em arquiteturas e formulários/gatitobook/src/app/home/novo-usuario/usuario-existe.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
-import { first, map, switchMap } from 'rxjs/operators';
 import { NovoUsuarioService } from './novo-usuario.service';
+import { first, map, switchMap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioExisteService {
+  constructor(private novoUsuarioService: NovoUsuarioService) {}
 
-  constructor(private novoUsuarioService: NovoUsuarioService) { }
-
-  usuarioExistente() {
+  usuarioJaExite() {
     return (control: AbstractControl) => {
       return control.valueChanges.pipe(
-        switchMap((nomeUsuario: any) =>
+        switchMap((nomeUsuario) =>
           this.novoUsuarioService.verificaUsuarioExistente(nomeUsuario)
         ),
-        map((usuarioExiste: any) =>
+        map((usuarioExiste) =>
           usuarioExiste ? { usuarioExistente: true } : null
         ),
         first()
-      )
-    }
+      );
+    };
   }
 }
